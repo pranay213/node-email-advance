8d49a1056baba74659dd3357fe5adb3400ce3846
pranaykodam <pranaykodam.213@gmail.com>
2026-02-08 17:34:18 +0530

    new changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import contactRoutes from './src/routes/contact.routes.js';
 import configRoutes from './src/routes/config.routes.js';
 import mailService from './src/services/mail.service.js';
 import dbService from './src/services/db.service.js';
-import { DatabaseFactory } from './src/services/database.factory.js';
+import DatabaseFactory from './src/services/database.factory.js';
 
 export {
     contactRoutes,
